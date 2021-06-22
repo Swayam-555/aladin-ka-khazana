051ce6789957914fee853eede86f2ecb21d2a24a
Swayam-555 <radheshyam.panigrahi@gmail.com>
2021-06-22 17:20:05 +0530

    aladin

--- js/Code.js
+++ js/Code.js
@@ -1,6 +1,6 @@
 const accessCode1 = "VARIABLE";
-const accessCode2 = "FUNCTION";
-const accessCode3 = "DATABASE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
 
 function clues() {
     
@@ -10,6 +10,16 @@ function clues() {
     fill("lightblue")
     text("Hint: Always changing, not constant !!", 100,70)
 // add code for displaying rest of the names and the hints.
-    
+fill("white")
+textSize(15)
+text("A T A D E S B A", 700,150)
+fill("lightblue")
+text("Hint: Always changing, not constant !!", 700,170)
+
+fill("white")
+textSize(15)
+text("C U T N I F O N", 100,320)
+fill("lightblue")
+text("Hint: Always changing, not constant !!", 100,340)
 
 }
